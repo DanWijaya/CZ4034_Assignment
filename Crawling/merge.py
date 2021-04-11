a893b98c44e7bb3b17fb0b4ca184fbec7fb2a330
ChenCheng0630 <chencheng991207@gmail.com>
2021-04-11 16:40:58 +0800

    added metrics

--- Crawling/merge.py
+++ Crawling/merge.py
@@ -10,14 +10,8 @@ def add_click_counts(review_csv):
 
 
 def main():
-    product_csv = pd.read_csv("product_lists.csv")
-    result_csv = pd.read_csv("result.csv")
-    review_csv = pd.read_csv("review_table.csv")
-    print(add_click_counts(review_csv).to_csv("review_table_click_counts.csv", index=False))
-
-    # print(len(review_csv))
-    # print(len(product_csv))
-    # print(result_csv["product_id"].value_counts())
+    review_csv = pd.read_csv("review_table_edited.csv")
+    print(review_csv.head())
 
 
 if __name__ == "__main__":
